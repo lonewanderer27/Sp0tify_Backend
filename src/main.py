1a05e8bb71c3fe8ac67bbdc020e2a7a860b0111f
lonewanderer27 <adrianejames27@gmail.com>
2023-08-07 17:23:02 +0800

    feat: require use of space app key

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security.api_key import APIKeyHeader
 from .routers import charts, search, song, album, lyrics, video, artist
 import uvicorn
 import os
 
-sp0tify = FastAPI(title="Sp0tify API")
+X_SPACE_APP_KEY = APIKeyHeader(name="X-Space-App-Key")
+
+
+def check_app_key(x_space_app_key: str = Depends(X_SPACE_APP_KEY)):
+    if x_space_app_key == "" or x_space_app_key == None:
+        return False
+    else:
+        return True
+
+
+sp0tify = FastAPI(title="Sp0tify API",
+                  description="Sp0tify API is a free API that allows you to get information about songs, artists, albums, lyrics, videos, and charts.",
+                  version="1.0.0")
 
 sp0tify.add_middleware(
     CORSMiddleware,
@@ -14,18 +28,18 @@ sp0tify.add_middleware(
     allow_headers=["*"]
 )
 
-sp0tify.include_router(search.router)
-sp0tify.include_router(song.router)
-sp0tify.include_router(artist.router)
-sp0tify.include_router(album.router)
-sp0tify.include_router(lyrics.router)
-sp0tify.include_router(video.router)
-sp0tify.include_router(charts.router)
+sp0tify.include_router(search.router, dependencies=[Depends(check_app_key)])
+sp0tify.include_router(song.router, dependencies=[Depends(check_app_key)])
+sp0tify.include_router(artist.router, dependencies=[Depends(check_app_key)])
+sp0tify.include_router(album.router, dependencies=[Depends(check_app_key)])
+sp0tify.include_router(lyrics.router, dependencies=[Depends(check_app_key)])
+sp0tify.include_router(video.router, dependencies=[Depends(check_app_key)])
+sp0tify.include_router(charts.router, dependencies=[Depends(check_app_key)])
 
 
 @sp0tify.get("/")
 async def root():
-    return {"message": "Sp0tify API"}
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run(sp0tify, host="0.0.0.0", port=int(
