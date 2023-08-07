207a318a261aa0571fa32413a585c4522900ee05
lonewanderer27 <adrianejames27@gmail.com>
2023-08-07 18:04:37 +0800

    chore: video route description

--- src/routers/video.py
+++ src/routers/video.py
@@ -6,13 +6,13 @@ from typing import Annotated, Union, Optional
 router = APIRouter(tags=["Video"])
 
 
-@router.get("/video", dependencies=[Depends(verify_id)])
+@router.get("/video", dependencies=[Depends(verify_id)], description="Gets data for a specific video.")
 async def search_video(id: int):
     res = genius.video(id)
     return res
 
 
-@router.get("/videos")
+@router.get("/videos", description="Gets the videos of an album, article or song or the featured videos.")
 async def search_videos(
         album_id=None, article_id=None, song_id=None, video_id=None,
         per_page: Annotated[Union[int, None], Query(
